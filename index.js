dd84bdc91c6f9ceb3b81233c66310b1413ab288d
Stoo Johnston <scj@stooj.org>
2024-10-13 21:03:01 +0200

    use predictable name for repo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require("@pulumi/github")
 
 const pulumiRepo = new github.Repository("pulumi-higara-repo", {
     description: "Repo for pulumi higara configuration",
+    name: "pulumi-higara",
 } {
     retainOnDelete: true
 });
