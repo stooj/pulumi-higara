8fb3f6ecf6c3fee7920e4f0fdaaa9d58ba9b2561
stoo johnston <scj@stooj.org>
2025-05-10 12:06:26 +0200

    Add flake-utils input

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
   description = "Development environment for higara pulumi project";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = { self }: { };
 }
